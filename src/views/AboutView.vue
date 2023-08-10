7fcc777255bfca2cfe72a3856fe80c87945a35fc
ptpofficialxd <aekkungzaza@gmail.com>
2023-08-11 03:18:31 +0700

    setup project / add extensions

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="lg:min-h-screen lg:flex lg:items-center">
+  <div class="about font-bold text-5xl">
     <h1>This is an about page</h1>
   </div>
 </template>
 
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 70vh;
+    display: flex;
+    justify-content: center; /* Center horizontally */
+    align-items: center; /* Center vertically */
+  }
+}
+</style>
